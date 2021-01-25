a48a685741c8bcc3430c4f22451c8e73055cb95a
Tom Gibbons <tgibbons@css.edu>
2021-01-24 20:10:43 -0600

    Initial commit

---
+++ app/src/main/java/tgibbons/diceroller_s2021/MainActivity.java
@@ -0,0 +1,33 @@
+package tgibbons.diceroller_s2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnRoll;
+    TextView tvDieValue;
+    Dice die;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        die = new Dice();
+        tvDieValue = findViewById(R.id.textViewDie);
+        btnRoll = findViewById(R.id.buttonRoll);
+
+        btnRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //btnRoll.setText(die.roll(6).toString());
+                tvDieValue.setText(die.roll(6).toString());
+            }
+        });
+    }
+}
