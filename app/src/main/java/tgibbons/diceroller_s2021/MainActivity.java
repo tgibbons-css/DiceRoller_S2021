5d70c01ad27018c0586bf514163284aa498223d6
Tom Gibbons <tgibbons@css.edu>
2021-02-02 09:51:18 -0600

    Added comment.

--- app/src/main/java/tgibbons/diceroller_s2021/MainActivity.java
+++ app/src/main/java/tgibbons/diceroller_s2021/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvDieValue;
     Dice die;
 
-    // sample comment
+    // New lines
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
